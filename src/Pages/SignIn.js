b33ecb2156aa936d75ef5a2fe4f235124e17dace
Temurbek Sabirov <prodota150@gmail.com>
2020-06-17 14:50:21 -0400

    Move SignIn and SignUp styles to separate file

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -10,30 +10,10 @@ import {
   Grid,
   LockOutlinedIcon,
   Typography,
-  makeStyles,
   Container,
 } from "../MaterialUI";
 import PasswordForget from "../Components/PasswordForget/Index";
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import useStyles from "../config/theme.SignIn";
 
 const SignIn = (props) => {
   const classes = useStyles();
