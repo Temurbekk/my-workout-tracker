41160f800b6f23646f21294203f0108c5d23837c
king <tettehmel@gmail.com>
2020-06-18 08:52:26 -0400

    bug: deleted import app.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./config/theme.config";
 
-import "./App.css";
+
 import Routes from "./Components/Routes";
 
 function App() {
