1f5f443e646c5219a5ac7194108247687988075b
Sahal Asghar <sahaldos1@gmail.com>
2020-06-18 11:49:46 -0400

    add dummy youtube page components

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  state ={
+    searchTerm : '',
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>This is a Search Bar components</h1>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
